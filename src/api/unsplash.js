3be922a8ad6eb8ad8c0027cdcc767955a96d13c2
rajesh1202 <rajeshakkineni4@gmail.com>
2021-03-25 20:30:11 -0400

    Pics by search Project

---
+++ src/api/unsplash.js
@@ -0,0 +1,8 @@
+import axiox from "axios";
+
+export default axiox.create({
+  baseURL: "https://api.unsplash.com",
+  headers: {
+    Authorization: "Client-ID PICzKUvbpD-dS5-u0Rz60yi31fXRtfQyUE6v9hZBfUw",
+  },
+});
